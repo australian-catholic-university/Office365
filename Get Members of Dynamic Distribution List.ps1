7beed0e5f39d0f6dbc70c9c4fd26f39c09e8b4d9
AdamFrohloff <51187444+AdamFrohloff@users.noreply.github.com>
2019-06-10 13:54:04 +1000

    Add files via upload

---
+++ Get Members of Dynamic Distribution List.ps1
@@ -0,0 +1,10 @@
+﻿$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking
+
+$DynamicDL = "DL-name@acu.edu.au"
+
+$members = Get-DynamicDistributionGroup -Identity $DynamicDL
+Get-Recipient -RecipientPreviewFilter $members.RecipientFilter | select Displayname,PrimarySmtpAddress
+
+Remove-PSSession $Session
