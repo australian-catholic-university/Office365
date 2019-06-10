42ca78b36489c43f2bf79c1e58e07a57b3c0d5b9
AdamFrohloff <51187444+AdamFrohloff@users.noreply.github.com>
2019-06-10 12:33:31 +1000

    Add files via upload

---
+++ Enable Mailbox Auditing.ps1
@@ -0,0 +1,14 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking
+
+# Finds mailboxes without auditing and turns it on
+
+$Mailboxes = (Get-Mailbox -Resultsize unlimited | where-object {$_.AuditEnabled -ne $true}).Name
+foreach ($Mailbox in $Mailboxes) {
+Set-Mailbox -Identity $Mailbox -AuditEnabled $true -AuditLogAgeLimit 180 -AuditAdmin Update, MoveToDeletedItems, SoftDelete, HardDelete, SendAs, SendOnBehalf, Create, UpdateFolderPermission -AuditDelegate Update, SoftDelete, HardDelete, SendAs, Create, UpdateFolderPermissions, MoveToDeletedItems, SendOnBehalf -AuditOwner UpdateFolderPermission, MailboxLogin, Create, SoftDelete, HardDelete, Update, MoveToDeletedItems 
+Write-Output $Mailbox
+}
+
+Remove-PSSession $Session
